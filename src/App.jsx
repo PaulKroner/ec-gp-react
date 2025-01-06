4d05a5065bbe6aa97299d76ca42bbc533670177f
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-01-06 10:17:47 +0100

    deleted unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css';
-import { useToast } from "./hooks/use-toast"
 import Dashboard from './pages/dashboard/dashboard';
 
 function App() {
-  const { toast } = useToast()
   return (
     <main className="App">
       <Dashboard />
