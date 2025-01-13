c7652cf7ccde3dbbe34e42f17c302471f9008573
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-01-13 14:47:40 +0100

    added test page not working

---
+++ src/pages/wordpressTest/WordpressTest.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const WordPressTest = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost/wordpress/wp-json/react-api/v1/get-data")
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data); // Check if data is received
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>WordPress Daten</h1>
+      {error ? (
+        <p>Fehler: {error}</p>
+      ) : (
+        <ul>
+          {data.map((item) => (
+            <li key={item.ID}>
+              <strong>{item.post_title}</strong> - {item.post_date}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default WordPressTest;
