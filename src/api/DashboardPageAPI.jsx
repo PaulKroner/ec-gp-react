e1dae4f47c2e764d4a87ff044e832a8a68d55529
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-02-03 15:21:59 +0100

    #7 added route and made it work

--- src/api/DashboardPageAPI.jsx
+++ src/api/DashboardPageAPI.jsx
@@ -2,26 +2,26 @@ import axiosInstanceAPI from "./axiosInstanceAPI";
 
 export const InsertEmployee = async (transformedFormData, toast, setLoading) => {
   setLoading(true);
-  
+
+
   try {
-    const response = await axiosInstanceAPI.post('/create', transformedFormData, {
+    const response = await axiosInstanceAPI.post('/insertEmployee.php', transformedFormData, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     // Check if fz_eingetragen is empty before sending the email
-    if (transformedFormData.fz_eingetragen === null) {
-      await axiosInstanceAPI.post('sendEmailRequestFZ', {
-        email: transformedFormData.email,
-        name: transformedFormData.name
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    }
-
+    // if (transformedFormData.fz_eingetragen === null) {
+    //   await axiosInstanceAPI.post('sendEmailRequestFZ', {
+    //     email: transformedFormData.email,
+    //     name: transformedFormData.name
+    //   }, {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //   });
+    // }
     window.location = "/dashboard"; // workaround to refresh the page
     toast({
       description: "Neuen Mitarbeiter erfolgreich hinzugefügt.",
--- src/api/axiosInstanceAPI.js
+++ src/api/axiosInstanceAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstanceAPI = axios.create({
   // baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost/php-backend/api",
-  baseURL: "http://localhost/php-backend/api",
+  baseURL: "http://localhost/gewaltprävention-edv/php-backend/api",
   headers: {
     'Content-Type': 'application/json',
   },
