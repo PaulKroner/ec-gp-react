54aa29d325557e25e1e9a3fd34205a6de047a132
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-02-03 09:19:33 +0100

    deleted unused wordpress code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ResetPasswordRequest from './pages/registration/resetPasswordRequest/ResetPasswordRequest';
 import ResetPassword from './pages/registration/resetPassword/[token]/ResetPassword';
-import WordpressTest from './pages/wordpressTest/WordpressTest';
+import FetchUsers from './pages/text';
 
 function App() {
   return (
@@ -27,7 +27,7 @@ function App() {
         <Route path="/not-found" element={<NotFoundPage />} />
         <Route path="/registration/resetPasswordRequest" element={<ResetPasswordRequest />} />
         <Route path="/registration/resetPassword/:token" element={<ResetPassword />} />
-        <Route path="/wordpressTest" element={<WordpressTest />} />
+        <Route path="/test" element={<FetchUsers />} />
       </Routes>
       </AuthProvider>
     </>
