c6b0084fa7cc103a72e84c9f4acd609f9409a6cd
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-02-04 15:25:09 +0100

    added user getData and delete route

--- src/api/UserAdministrationAPI.jsx
+++ src/api/UserAdministrationAPI.jsx
@@ -4,7 +4,7 @@ import axiosInstanceAPI from './axiosInstanceAPI';
 // Function to get users from the backend
 export const getDataRoles = async () => {
   try {
-    const response = await axiosInstanceAPI.get('/getDataRoles');
+    const response = await axiosInstanceAPI.get('/getUserData.php');
     return response.data; // Return the fetched data
   } catch (error) {
     return null; // Return null or handle the error appropriately
@@ -13,7 +13,7 @@ export const getDataRoles = async () => {
 
 export const deleteUserFromDB = async (id, toast, data, setData) => {
   try {
-    await axiosInstanceAPI.delete(`/deleteUser/${id}`);
+    await axiosInstanceAPI.delete(`/deleteUser.php?id=${id}`);
     setData(data.filter(employee => employee.id !== id));
     toast({
       description: "Mitarbeiter wurde erfolgreich gelöscht.",
