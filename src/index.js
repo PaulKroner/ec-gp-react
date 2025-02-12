d637784d82f3f413ebd9158757e584389bc027c3
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-02-12 16:59:29 +0100

    changed url for prod

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Toaster } from "components/ui/toaster";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/gewaltpraevention-edv/frontend">
       <App />
     </BrowserRouter>
     <Toaster />
