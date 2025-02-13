246ab305cc7f0630bf7f9c24fc7615a2f17a9e9b
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-02-13 14:57:52 +0100

    changed baseURL

--- src/api/axiosInstanceAPI.js
+++ src/api/axiosInstanceAPI.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const axiosInstanceAPI = axios.create({
-  // baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost/php-backend/api",
-  baseURL: "http://localhost/gewaltpraevention-edv/php-backend/api",
+  baseURL: process.env.REACT_APP_GP_EDV_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,  // Fügt Cookies in Anfragen hinzu
+  withCredentials: true,
 });
 
 export default axiosInstanceAPI;
