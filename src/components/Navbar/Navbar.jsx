0477e948f21ae53633700316339659e922455c15
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-03-24 18:25:31 +0100

    changed navbar buttons and logout button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
         <div className="flex flex-col items-center justify-between max-w-full px-4 md:flex-row">
 
           <div className="flex items-center order-2 mt-4 sm:mt-0">
-            <div className="flex flex-row items-center gap-1.5 bg-blue-500 text-white py-2 px-4 rounded-3xl text-2xl font-semibold disabled:opacity-50" onClick={Logout} disabled={loading}>
+            <button className="flex flex-row items-center gap-1.5 bg-blue-500 text-white py-2 px-4 rounded-3xl text-2xl font-semibold disabled:opacity-50" onClick={Logout} disabled={loading}>
               {loading ? (
                 <>
                   <LoadingSpinner spinnerWidht={"w-6"} spinnerHeight={"h-6"} color={"text-white"} />
@@ -38,7 +38,7 @@ const NavBar = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
                 </svg>
               </div>
-            </div>
+            </button>
           </div>
 
           <div className="items-center justify-center w-full flex sm:w-auto order-1 mt-4 sm:mt-0" id="mobile-menu-2">
@@ -47,9 +47,9 @@ const NavBar = () => {
                 <li>
                   <Link
                     to="/registration"
-                    className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 lg:hover:bg-transparent lg:border-0 hover:text-blue-500 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 pl-3 pr-4 text-gray-700 lg:hover:bg-transparent lg:border-0 hover:text-blue-500 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    <Button variant="outline" className="w-48 hover:text-blue-500">
+                    <Button variant="" className="w-48">
                       Benutzer registrieren
                     </Button>
                   </Link>
@@ -62,9 +62,9 @@ const NavBar = () => {
                 <li>
                   <Link
                     to="/userAdministration"
-                    className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 lg:hover:bg-transparent lg:border-0 hover:text-blue-500 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 pl-3 pr-4 text-gray-700 lg:hover:bg-transparent lg:border-0 hover:text-blue-500 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    <Button variant="outline" className="w-48 hover:text-blue-500">
+                    <Button variant="" className="w-48">
                       Benutzer verwalten
                     </Button>
                   </Link>
