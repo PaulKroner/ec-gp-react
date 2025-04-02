334859e02f7e0cf62ad3ce43cca9f031729d77f7
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-04-02 18:35:08 +0200

    Wenn neuer Mitarbeiter hinzugefügt wird #37 fixed error 500

--- src/api/DashboardPageAPI.jsx
+++ src/api/DashboardPageAPI.jsx
@@ -10,40 +10,35 @@ export const InsertEmployee = async (transformedFormData, toast, setLoading) => 
       },
     });
 
-    // Check before sending the email
     const sendEmailRequest = async (endpoint, successMessage) => {
-      try {
-        await axiosInstanceAPI.post(endpoint, {
-          email: transformedFormData.email,
-          name: transformedFormData.name,
-          vorname: transformedFormData.vorname
-        }, {
-          headers: { 'Content-Type': 'application/json' },
-        });
-    
+      return axiosInstanceAPI.post(endpoint, {
+        email: transformedFormData.email,
+        name: transformedFormData.name,
+        vorname: transformedFormData.vorname
+      }, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(() => {
         toast({ description: successMessage });
-      } catch (emailError) {
+      })
+      .catch((emailError) => {
         toast({
           variant: "destructive",
           description: emailError.response?.data?.message || "Fehler beim Senden der E-Mail.",
         });
-      }
+      });
     };
     
     if (transformedFormData.fz_eingetragen === null) {
       await sendEmailRequest('/sendEmailRequestFZ.php', 
         "E-Mail zur Führungszeugnis-Anforderung erfolgreich gesendet.");
     }
-    
+
     if (transformedFormData.sve_eingetragen === null) {
       await sendEmailRequest('/SendEmailRequestSVE.php', 
         "E-Mail zur Selbstverpflichtungserklärung-Anforderung erfolgreich gesendet.");
     }
 
-    window.location = `/dashboard`; // workaround to refresh the page -> problem: toast does not get rendered correctly
-    toast({
-      description: "Neuen Mitarbeiter erfolgreich hinzugefügt.",
-    });
   } catch (error) {
     if (error.response) {
       toast({
@@ -57,6 +52,6 @@ export const InsertEmployee = async (transformedFormData, toast, setLoading) => 
       });
     }
   }
-  
+
   setLoading(false);
 };
